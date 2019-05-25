a992ffd990d0d1c7fba0cf3b9ad33d6f0d8dc64b
Pruthvi Raj <scarydonut@github.com>
2019-05-25 09:12:28 +0530

    updated tube

--- angiogram/angiogram.cpp
+++ angiogram/angiogram.cpp
@@ -3,7 +3,7 @@
 
 float start = 21.0;
 float ym = 21.0;
-float y[] = {start,start-3,start-6,start-9};
+float y[] = { start,start - 3,start - 6,start - 9 };
 float yw = 11.0;
 float dec = 0.0;
 float thick = 0.010;
@@ -12,7 +12,8 @@ float HEIGHT = 400.0;
 float vesselLength = 20.0;
 float cellLength = 1.0;
 int count = 0;
-int normal = 1, mesh = 0, block = 0;
+int normal = 1, mesh = 0, block = 0, whitetube = 0;
+
 
 GLsizei ww = 1536;
 GLsizei wh = 801;
@@ -496,49 +497,27 @@ void veins() {
 
 }
 
-void display() {
-	//vector<int> heart{ 640,850,620,800,600,750,600,650,630,550,680,500,750,460,850,440,1000,500 };
-	// int start1x = 400, start1y = 200;
-	// int start2x = 800, start2y = 200;
-	// int t1x = 400, t1y = 350, t2x = 800, t2y = 350;
-	// glPointSize(5.0);
-	//glColor3b(1.0, 1.0, 0.0);
-	// glLineWidth(2.5);
-	// glBegin(GL_LINE_STRIP);
-	/*for (std::vector<int>::size_type i = 0; i != heart.size()-1; i+=2) {
-		glVertex2d(heart[i], heart[i + 1]);
-	}*/
-	// glColor3f(1.0, 0.5, 0.5);
-	//int t = 30;
-	/*
-	glVertex2d(440, 650);
-	glVertex2d(420, 600);
-	glVertex2d(400, 550);
-	glVertex2d(400, 450);
-	glVertex2d(400, 450);
-	glVertex2d(430, 350);
-	glVertex2d(480, 300);
-	glVertex2d(550, 260);
-	glVertex2d(650, 240);
-	glVertex2d(800, 300);
-	//glVertex2d(1020, 550);
-	glVertex2d(820, 400);
-	glVertex2d(780, 500);
-	glVertex2d(680, 600);
-	glVertex2d(600, 640);
-	glVertex2d(440, 650);
-	*/
+void puttube() {
+	if (whitetube == 1) {
+		glLineWidth(15);
+		glColor4f(1.0, 1.0, 1.0, 0.5);
+		int t = 10;
+		glBegin(GL_LINES);
+		glVertex2f(500, 500);
+		glVertex2f(500, 550);
+		glVertex2f(500, 570);
+		glVertex2f(500, 600);
+		glVertex2f(510, 630);
+		glVertex2f(550, 655);
+		glVertex2f(560, 660);
+		glVertex2f(650, 700);
 
-	/*for (int i = 0; i <= t; i++) {
-		float pos = (float)i / (float)t;
-		GLfloat x = bezierCurve(pos, start1x, t1x, t2x, start2x);
-		GLfloat y = bezierCurve(pos, start2y, t1y, t2y, start2y);
-		glVertex2f(x, y);
+		glEnd();
 	}
-	*/
-	//glVertex2d(100, 200);
-	//glVertex2d(400, 200);
-	// glEnd();
+}
+
+void display() {
+
 	shape();
 
 	//tubes right side
@@ -557,37 +536,26 @@ void display() {
 
 	aortaTubes();
 
+	puttube();
 	//glVertex2f(450, 350);
 	glFlush();
 }
-/*void reshape(GLsizei w, GLsizei h) {
-	glViewport(0, 0, (GLdouble)w, (GLdouble)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	// glOrtho scales the matrix by multiplying the matrix
-	if (w <= h) {
-		glOrtho(-5.0, 5.0, -5.0*(GLdouble)h / (GLdouble)w,
-			5.0*(GLdouble)h / (GLdouble)w, -5.0, 5.0);
-	}
-	else {
-		glOrtho(-5.0*(GLdouble)w / (GLdouble)h,
-			5.0*(GLdouble)w / (GLdouble)h, -5.0, 5.0, -5.0, 5.0);
-	}
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	ww = w;
-	wh = h;
-}*/
 
 void keyboardPress(unsigned char key, int x, int y) {
-	if (key == 'c' || key == 'C') {
+	if (whitetube == 1 && (key == 'c' || key == 'C')) {
 		color[0] = 0.0;
 	}
 
-	if (key == 'r' || key == 'R') {
+	if (whitetube == 1 && (key == 'r' || key == 'R')) {
 		color[0] = 1.0;
 	}
+
+	if (key == 't' || key == 'T') {
+		whitetube = 1;
+	}
+
 	glutPostRedisplay();
+
 }
 
 void reshape(GLsizei w, GLsizei h) {
@@ -616,213 +584,214 @@ void init() {
 }
 
 void init2() {
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(60, (float)WIDTH/HEIGHT, 0.01f, 100.0f);
-  glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, (float)WIDTH / HEIGHT, 0.01f, 100.0f);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void bloodFlow(int value) {
-  int i = 0;
-  if (y[i] != -10.0) {
-      y[i] -= 1.0;
-      y[i+1] -= 1.0;
-      y[i+2] -= 1.0;
-      y[i+3] -= 1.0;
-  }
-  glutPostRedisplay();
-  glutTimerFunc(400, bloodFlow, 0);
+	int i = 0;
+	if (y[i] != -10.0) {
+		y[i] -= 1.0;
+		y[i + 1] -= 1.0;
+		y[i + 2] -= 1.0;
+		y[i + 3] -= 1.0;
+	}
+
+	glutPostRedisplay();
+	glutTimerFunc(200, bloodFlow, 0);
 }
 
 void putMesh(int value) {
-  if (ym != 1.0)
-    ym -= 1.0;
-  glutPostRedisplay();
-  glutTimerFunc(400, putMesh, 0);
+	if (ym != 1.0)
+		ym -= 1.0;
+	glutPostRedisplay();
+	glutTimerFunc(400, putMesh, 0);
 }
 
 void reduceFat(int value) {
-  if (yw != 7.0)
-    yw -= 1.0;
-  else{
-    if(dec <= 0.1 && count <= 5){
-      thick += 0.015;
-      dec += 0.005;
-      count += 1;
-      if (count == 5){
-        thick = 0.010;
-        count = 0;
-      }
-    }
-    else
-      return;
-  }
-  glutPostRedisplay();
-  glutTimerFunc(400, reduceFat, 0);
+	if (yw != 7.0)
+		yw -= 1.0;
+	else {
+		if (dec <= 0.1 && count <= 5) {
+			thick += 0.015;
+			dec += 0.005;
+			count += 1;
+			if (count == 5) {
+				thick = 0.010;
+				count = 0;
+			}
+		}
+		else
+			return;
+	}
+	glutPostRedisplay();
+	glutTimerFunc(400, reduceFat, 0);
 }
 
-void drawVessel(){
+void drawVessel() {
 	glPushMatrix();
-  glColor3f(0.0,0,0.5);
-  GLUquadricObj *quadratic;
-  quadratic = gluNewQuadric();
-  glScalef(0.15,0.1,0.0);
-  glTranslatef(0.0,7.0,0.0);
-  glRotatef(45.0, 1.0, 0.0, 0.0);    
-  gluCylinder(quadratic,0.5,0.5,vesselLength,32,32);
+	glColor3f(0.0, 0, 0.5);
+	GLUquadricObj *quadratic;
+	quadratic = gluNewQuadric();
+	glScalef(0.15, 0.1, 0.0);
+	glTranslatef(0.0, 7.0, 0.0);
+	glRotatef(45.0, 1.0, 0.0, 0.0);
+	gluCylinder(quadratic, 0.5, 0.5, vesselLength, 32, 32);
 	glPopMatrix();
 	glutSwapBuffers();
 }
 
-void DrawCircle(float cx, float cy, float r, int num_segments, float tx, float ty, float sx, float sy, float theta){
-  glPushMatrix();
-  glScalef(sx,sy,0.0);
-  glTranslatef(tx,ty,0.0);
-  glRotatef(theta, 0.0, 0.0, 10.0);
-  glBegin(GL_TRIANGLE_FAN);
-  if (r >= 0){
-    for(int ii = 0; ii < num_segments; ii++){
-        float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);
-        float x = r * cosf(theta/2);
-        float y = r * sinf(theta/2);
-        glVertex2f(x + cx, y + cy);
-    }
-  }
-  glEnd();
-  glPopMatrix();
+void DrawCircle(float cx, float cy, float r, int num_segments, float tx, float ty, float sx, float sy, float theta) {
+	glPushMatrix();
+	glScalef(sx, sy, 0.0);
+	glTranslatef(tx, ty, 0.0);
+	glRotatef(theta, 0.0, 0.0, 10.0);
+	glBegin(GL_TRIANGLE_FAN);
+	if (r >= 0) {
+		for (int ii = 0; ii < num_segments; ii++) {
+			float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);
+			float x = r * cosf(theta / 2);
+			float y = r * sinf(theta / 2);
+			glVertex2f(x + cx, y + cy);
+		}
+	}
+	glEnd();
+	glPopMatrix();
 }
 
 void drawFat(float tx, float ty, float sx, float sy, float theta) {
-  glColor3f(1.0, 0.5, 0.0);
-  DrawCircle(0.5, 0.5, 0.2 - dec, 35, tx, ty, sx, sy, theta);
+	glColor3f(1.0, 0.5, 0.0);
+	DrawCircle(0.5, 0.5, 0.2 - dec, 35, tx, ty, sx, sy, theta);
 }
 
-void drawCell(int y){
-  glPushMatrix();
-  glColor3f(0.7,0,0);
-  glScalef(0.1,0.1,0.01);
-  glTranslatef(0.0,y,0.0);
-  glRotatef(45.0, 1.0, 0.0, 0.0);
-    GLUquadricObj *quadratic;
-    quadratic = gluNewQuadric();
-    gluCylinder(quadratic,0.5,0.5,cellLength,10,10);
-    glPopMatrix();
-    glutSwapBuffers();
-}
-
-void drawWire(){
-  glPushMatrix();
-  glColor3f(1.0,1.0,1.0);
-  GLUquadricObj *quadratic;
-  quadratic = gluNewQuadric();
-  glScalef(0.005,0.1,0.0);
-  glTranslatef(0.0,yw,0.0);
-  glRotatef(45.0, 1.0, 0.0, 0.0);    
-  gluCylinder(quadratic,0.5,0.5,vesselLength,32,32);
-  glPopMatrix();
-  glutSwapBuffers();
-}
-
-void drawBalloon(){
-  glPushMatrix();
-  glColor3f(1.0,1.0,1.0);
-  GLUquadricObj *quadratic;
-  quadratic = gluNewQuadric();
-  glScalef(0.010 + thick,0.1,0.0);
-  glTranslatef(0.0,yw+5,0.0);
-  glRotatef(45.0, 1.0, 0.0, 0.0);    
-  gluCylinder(quadratic,0.5,0.5,vesselLength,32,32);
-  glPopMatrix();
-  glutSwapBuffers();  
-}
-
-void drawMesh(){
+void drawCell(int y) {
 	glPushMatrix();
-  glColor3f(0.7,0.7,0.7);
-  GLUquadricObj *quadratic;
-  quadratic = gluNewQuadric();
-	gluQuadricDrawStyle(quadratic, GLU_LINE); /* flat shaded */
- 	gluQuadricNormals(quadratic, GLU_SMOOTH);
-  glScalef(0.15,0.1,0.0);
-  glTranslatef(0.0,ym,0.0);
-  glRotatef(45.0, 1.0, 0.0, 0.0);    
-  gluCylinder(quadratic,0.5,0.5,vesselLength/6,32,32);
+	glColor3f(0.7, 0, 0);
+	glScalef(0.1, 0.1, 0.01);
+	glTranslatef(0.0, y, 0.0);
+	glRotatef(45.0, 1.0, 0.0, 0.0);
+	GLUquadricObj *quadratic;
+	quadratic = gluNewQuadric();
+	gluCylinder(quadratic, 0.5, 0.5, cellLength, 10, 10);
 	glPopMatrix();
-	glutSwapBuffers();	
+	glutSwapBuffers();
 }
 
-void display2(){
-  glClear(GL_COLOR_BUFFER_BIT);
-  if(normal){
-    drawVessel();
-    int i = 0;
-    while (i < 4){
-      drawCell(y[i]);
-      i+=1;
-    }
-  }
-  if(mesh){
-    drawVessel();
-    drawFat(-.81, 0.49, 0.25, 1.0, -90.0);
-    drawFat(.81, -0.49, 0.25, 1.0, 90.0);
-    drawMesh();      
-    int i = 0;         
-    while (i < 4){
-      drawCell(y[i]);
-      i+=1;
-    }
-  }
-  if(block){
-    drawVessel();
-    drawFat(-.81, 0.49, 0.25, 1.0, -90.0);
-    drawFat(.81, -0.49, 0.25, 1.0, 90.0);
-    drawWire();
-    drawBalloon();
-  }
-  glFlush();
+void drawWire() {
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
+	GLUquadricObj *quadratic;
+	quadratic = gluNewQuadric();
+	glScalef(0.005, 0.1, 0.0);
+	glTranslatef(0.0, yw, 0.0);
+	glRotatef(45.0, 1.0, 0.0, 0.0);
+	gluCylinder(quadratic, 0.5, 0.5, vesselLength, 32, 32);
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
-void keyboardEvent(unsigned char key, int xc, int yc){
-  switch(key){
-    case 'm' : block = 0;
-               normal = 0;
-               mesh = 1;
-               glutTimerFunc(300, putMesh, 0);               
-               break;
-
-    case 'n' : block = 0;
-               mesh = 0;
-               normal = 1;
-               glutTimerFunc(300, bloodFlow, 0);
-               y[0] = start; y[1] = start - 3; y[2] = start - 6; y[3] = start - 9;
-               break;
-
-    case 'b' : block = 0;
-               normal = 0;
-               mesh = 1;
-               glutTimerFunc(300, bloodFlow, 0);
-               y[0] = start; y[1] = start - 3; y[2] = start - 6; y[3] = start - 9;
-               break;
-
-    case 'f' : mesh = 0;
-               normal = 0;
-               block = 1;
-               glutTimerFunc(300, reduceFat, 0);
-
-    default : break;
-  }
+void drawBalloon() {
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
+	GLUquadricObj *quadratic;
+	quadratic = gluNewQuadric();
+	glScalef(0.010 + thick, 0.1, 0.0);
+	glTranslatef(0.0, yw + 5, 0.0);
+	glRotatef(45.0, 1.0, 0.0, 0.0);
+	gluCylinder(quadratic, 0.5, 0.5, vesselLength, 32, 32);
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
-void mouseEvent(int button, int state, int x, int y){
-	if(button == GLUT_MIDDLE_BUTTON && state ==  GLUT_UP){
-		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);	
-    glutInitWindowPosition(100, 100);
-	  glutInitWindowSize(WIDTH, HEIGHT);
-	  init2();
-	  glutCreateWindow("Inside");
-	  glutDisplayFunc(display2);	
-	  glutKeyboardFunc(keyboardEvent);
-	  glutMainLoop();
+void drawMesh() {
+	glPushMatrix();
+	glColor3f(0.7, 0.7, 0.7);
+	GLUquadricObj *quadratic;
+	quadratic = gluNewQuadric();
+	gluQuadricDrawStyle(quadratic, GLU_LINE); /* flat shaded */
+	gluQuadricNormals(quadratic, GLU_SMOOTH);
+	glScalef(0.15, 0.1, 0.0);
+	glTranslatef(0.0, ym, 0.0);
+	glRotatef(45.0, 1.0, 0.0, 0.0);
+	gluCylinder(quadratic, 0.5, 0.5, vesselLength / 6, 32, 32);
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void display2() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	if (normal) {
+		drawVessel();
+		int i = 0;
+		while (i < 4) {
+			drawCell(y[i]);
+			i += 1;
+		}
+	}
+	if (mesh) {
+		drawVessel();
+		drawFat(-.81, 0.49, 0.25, 1.0, -90.0);
+		drawFat(.81, -0.49, 0.25, 1.0, 90.0);
+		drawMesh();
+		int i = 0;
+		while (i < 4) {
+			drawCell(y[i]);
+			i += 1;
+		}
+	}
+	if (block) {
+		drawVessel();
+		drawFat(-.81, 0.49, 0.25, 1.0, -90.0);
+		drawFat(.81, -0.49, 0.25, 1.0, 90.0);
+		drawWire();
+		drawBalloon();
+	}
+	glFlush();
+}
+
+void keyboardEvent(unsigned char key, int xc, int yc) {
+	switch (key) {
+	case 'm': block = 0;
+		normal = 0;
+		mesh = 1;
+		glutTimerFunc(300, putMesh, 0);
+		break;
+
+	case 'n': block = 0;
+		mesh = 0;
+		normal = 1;
+		glutTimerFunc(300, bloodFlow, 0);
+		y[0] = start; y[1] = start - 3; y[2] = start - 6; y[3] = start - 9;
+		break;
+
+	case 'b': block = 0;
+		normal = 0;
+		mesh = 1;
+		glutTimerFunc(300, bloodFlow, 0);
+		y[0] = start; y[1] = start - 3; y[2] = start - 6; y[3] = start - 9;
+		break;
+
+	case 'f': mesh = 0;
+		normal = 0;
+		block = 1;
+		glutTimerFunc(300, reduceFat, 0);
+
+	default: break;
+	}
+}
+
+void mouseEvent(int button, int state, int x, int y) {
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
+		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+		glutInitWindowPosition(100, 100);
+		glutInitWindowSize(WIDTH + 100, HEIGHT + 100);
+		init2();
+		glutCreateWindow("Artery");
+		glutDisplayFunc(display2);
+		glutKeyboardFunc(keyboardEvent);
+		glutMainLoop();
 	}
 }
 
